2c4ccfdef888b9936055a08a100050523570ffec
Elio Hoyos <elio18david@gmail.com>
2025-06-18 15:19:26 -0500

    ejercicio Pedidos | Clases y objetos

---
+++ Clase y Objetos/Todo_Barato/src/todo_barato/Todo_Barato.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package todo_barato;
+
+import todo_barato.layer.view.PedidoView;
+
+/**
+ *
+ * @author ELIOT
+ */
+public class Todo_Barato {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PedidoView.main(null);
+    }
+    
+}
